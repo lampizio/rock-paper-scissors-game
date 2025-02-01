794e9b5366431816829d04eb3b69740ff7c2fcc8
lampizio <nikita030405@gmail.com>
2025-02-01 22:27:47 +0300

    feat: add logic to rock-paper-scissors game

---
+++ scripts/script.js
@@ -0,0 +1,99 @@
+const ITEMS = {
+    rock: "rock",
+    paper: "paper",
+    scissors: "scissors",
+}
+
+const score = {
+    win: 0,
+    lose: 0,
+    draw: 0,
+} 
+
+function playGame(n) {
+    for (let i = 0; i < n; i++) {
+        playRound();
+    }
+}
+
+function playRound() {
+    let computerChoice = getComputerChoice();
+    let userChoice = getUserChoice();
+
+    let userRoundStatus = getUserRoundStatus(computerChoice, userChoice);
+
+    processUserRoundStatus(userRoundStatus, computerChoice, userChoice);
+
+}
+
+function getUserRoundStatus(computerChoice, userChoice) {
+    if ((userChoice == ITEMS.rock && computerChoice == ITEMS.scissors) ||
+        (userChoice == ITEMS.paper && computerChoice == ITEMS.rock)    ||
+        (userChoice == ITEMS.scissors && computerChoice == ITEMS.paper)
+       ) {
+        return true;
+    } else if (userChoice == computerChoice) {
+        return null;
+    } else {
+        return false;
+    }
+}
+
+function processUserRoundStatus(userRoundStatus, computerChoice, userChoice) {
+    if(userRoundStatus) {
+        score.win++;
+        console.log(`You win\nComputer: ${computerChoice}\nYou: ${userChoice}`)
+    } else if (userRoundStatus === null) {
+        score.draw++;
+        console.log(`Draw\nComputer: ${computerChoice}\nYou: ${userChoice}`)
+    } else {
+        score.lose++;
+        console.log(`You lose\nComputer: ${computerChoice}\nYou: ${userChoice}`)
+    }
+}
+
+function getComputerChoice() {
+    let items = Object.values(ITEMS);
+    let randInt = parseInt(Math.random() * 3);
+    return items[randInt];
+}
+
+function getUserChoice() {
+    let choice;
+
+    do {
+    choice = prompt("Enter the item:\n- Rock/R/r\n- Paper/P/p\n- Scissors/S/s").toLowerCase();
+    } while (!isValidUserChoice(choice));
+
+    return transformUserChoice(choice);
+}
+
+function isValidUserChoice(choice) {
+    return ["rock", "r", "paper", "p", "scissors", "s"].includes(choice);
+}
+
+function transformUserChoice(choice) {
+    switch (choice) {
+        case "rock":
+        case "r":
+            return ITEMS.rock;
+
+        case "paper":
+        case "p":
+            return ITEMS.paper;
+
+        case "scissors":
+        case "s":
+            return ITEMS.scissors;
+    }
+}
+
+function main() {
+    let n = parseInt(prompt("Enter the number of rounds:"))
+
+    playGame(n);
+
+    console.table(score);
+}
+
+main();
