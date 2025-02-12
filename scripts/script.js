b4a269a58186d760bfa778f43f2da4673041eaa2
lampizio <nikita030405@gmail.com>
2025-02-13 00:30:22 +0300

    feat: change return values proccessUserRoundStatus func

--- scripts/script.js
+++ scripts/script.js
@@ -10,29 +10,31 @@ const SCORE = {
     draw: 0,
 } 
 
+const MAX_ROUNDS = 5;
+
 function playRound(event) {
-    let userChoice = getUserChoiceFromClass(event.target.className);
+    let userChoice = getUserChoiceFromClass(event.target.classList);
     let computerChoice = getComputerChoice();
+    console.log(`Computer choice: ${computerChoice}`)
 
     let userRoundStatus = getUserRoundStatus(computerChoice, userChoice);
 
-    let resultStr = processUserRoundStatus(userRoundStatus, computerChoice, userChoice);
+    let resultStr = processUserRoundStatus(userRoundStatus);
     displayRoundResults(resultStr);
 
 }
 
 function displayRoundResults(result) {
-    document.querySelector(".display-results").innerText= result;
+    document.querySelector(".round-status").innerText = result;
 }
 
 function getUserChoiceFromClass(className) {
-    switch (className) {
-        case 'rock':
-            return ITEMS.rock;
-        case 'paper':
-            return ITEMS.paper;
-        case 'scissors':
-            return ITEMS.scissors;
+    if (className.contains("rock")) {
+        return ITEMS.rock;
+    } else if (className.contains("paper")) {
+        return ITEMS.paper;
+    } else {
+        return ITEMS.scissors;
     }
 }
 
@@ -49,16 +51,16 @@ function getUserRoundStatus(computerChoice, userChoice) {
     }
 }
 
-function processUserRoundStatus(userRoundStatus, computerChoice, userChoice) {
+function processUserRoundStatus(userRoundStatus) {
     if(userRoundStatus) {
         SCORE.win++;
-        return `You win\nComputer: ${computerChoice}\nYou: ${userChoice}`;
+        return "You win!";
     } else if (userRoundStatus === null) {
         SCORE.draw++;
-        return `Draw\nComputer: ${computerChoice}\nYou: ${userChoice}`;
+        return "Draw!";
     } else {
         SCORE.lose++;
-        return `You lose\nComputer: ${computerChoice}\nYou: ${userChoice}`;
+        return "You lose ;(";
     }
 }
 
@@ -66,34 +68,6 @@ function getComputerChoice() {
     let items = Object.values(ITEMS);
     let randInt = parseInt(Math.random() * 3);
     return items[randInt];
-}
-
-function isValidUserChoice(choice) {
-    return ["rock", "r", "paper", "p", "scissors", "s"].includes(choice);
-}
-
-function transformUserChoice(choice) {
-    switch (choice) {
-        case "rock":
-        case "r":
-            return ITEMS.rock;
-
-        case "paper":
-        case "p":
-            return ITEMS.paper;
-
-        case "scissors":
-        case "s":
-            return ITEMS.scissors;
-    }
-}
-
-function getNumberOfRounds(number) {
-    do {
-        number = parseInt(prompt("Enter the number of rounds:"))
-    } while (isNaN(number) || number <= 0);
-
-    return number;
 }
 
 function main() {
