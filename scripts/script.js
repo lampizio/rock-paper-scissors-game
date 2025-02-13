f402abd75fa905cd0ee5b13d4a8cd040b20e22a1
lampizio <nikita030405@gmail.com>
2025-02-13 03:10:54 +0300

    feat: add design styles to the page

--- scripts/script.js
+++ scripts/script.js
@@ -20,13 +20,25 @@ function playRound(event) {
     let userRoundStatus = getUserRoundStatus(computerChoice, userChoice);
 
     let result = processUserRoundStatus(userRoundStatus, computerChoice);
-    displayRoundResults(result);
+    displayRoundResults(result, userRoundStatus);
 
 }
 
-function displayRoundResults(result) {
-    document.querySelector(".round-status").textContent = result[0];
+function displayRoundResults(result, userRoundStatus) {
+    document.querySelector(".round-status").innerHTML = result[0];
     document.querySelector(".computer-choice").textContent = "Computer chose: " + result[1].toUpperCase();
+    document.querySelector(".user-score").textContent = "You: " + SCORE.win;
+    document.querySelector(".computer-score").textContent = "Computer: " + SCORE.lose;
+    document.querySelector(".draw-score").textContent = "Draw: " + SCORE.draw;
+
+    if (userRoundStatus) {
+        document.querySelector("#win").play();
+    } else if (userRoundStatus === null) {
+        document.querySelector("#draw").play();
+    } else {
+        document.querySelector("#lose").play();
+    }
+
 }
 
 function getUserChoiceFromClass(className) {
@@ -55,13 +67,13 @@ function getUserRoundStatus(computerChoice, userChoice) {
 function processUserRoundStatus(userRoundStatus, computerChoice) {
     if(userRoundStatus) {
         SCORE.win++;
-        return ["You win!", computerChoice];
+        return ["<span style='color: green;'>You win!</span>", computerChoice];
     } else if (userRoundStatus === null) {
         SCORE.draw++;
-        return ["Draw!", computerChoice];
+        return ["<span style='color: gray;'>Draw</span>", computerChoice];
     } else {
         SCORE.lose++;
-        return ["You lose ;(", computerChoice];
+        return ["<span style='color: red;'>You lose ;(</span>", computerChoice];
     }
 }
 
